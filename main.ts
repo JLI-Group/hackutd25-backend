eb843b6e72930e948300e1d794b06a8fa6e293df
Ice <ice.baldivas@gmail.com>
2025-11-08 17:03:12 -0600

    init3

--- main.ts
+++ main.ts
@@ -1,18 +1,5 @@
 import { MongoClient } from 'npm:mongodb@6.1.0'
-import { load } from 'jsr:@std/dotenv'
-
-const env = await load({
-    envPath: '.env',
-    // optional: also export to the process environment (so Deno.env can read it)
-    export: true,
-})
-
-const client = new MongoClient(Deno.env.get('MONGO_CONNECTION')!)
-
-await client.connect()
 
 Deno.serve((_req) => {
     return new Response('Hello, World!')
 })
-
-client.close();
